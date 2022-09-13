9f92efbc23f11ae8029ddc820f3211fac3aa4483
speed1313 <speedtry13@icloud.com>
2022-09-13 16:10:30 +0900

    add healthz endpoint

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ line_bot_api = LineBotApi(os.environ['YOUR_CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['YOUR_CHANNEL_SECRET'])
 
 
+
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -35,6 +36,9 @@ def callback():
 
     return 'OK'
 
+@app.route("/healthz")
+def health():
+    return
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
