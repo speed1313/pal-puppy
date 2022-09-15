6212060aa5f8c1d064130b2c939aa70a465f6357
nagatani-aoi <224x218x@stu.kobe-u.ac.jp>
2022-09-15 21:29:55 +0900

    data input

--- tables.sql
+++ tables.sql
@@ -4,8 +4,8 @@ INSERT INTO MESSAGES
     (MESSAGE) 
 VALUES
 ("Impossible is nothing."),
-("Drive your dreams."), 
-("Donft be evil."),
+("Drive your dreams."),
+-- ("Donft be evil."),
 ("Think different."),
 ("Nobody is perfect."),
 ("Happiness requires struggle."),
@@ -17,16 +17,22 @@ VALUES
 ("WE ALL HAVE OUR HANG-UPS!"),
 ("They canft order me to stop dreaming."),
 ("Sometimes losing may become a great fortune later."),
-("One does not care to acknowledge the mistakes of onefs youth."),
-("He stole something quite preciousc your heart."),
-("Youfll stumble many times in the future, but when you do, each time youfll have more strength to bounce back."),
-("If you donft like where you are, change it. Youfre not a tree."),
+-- ("One does not care to acknowledge the mistakes of onefs youth."),
+-- ("He stole something quite preciousc your heart."),
+-- ("Youfll stumble many times in the future, but when you do, each time youfll have more strength to bounce back."),
+-- ("If you donft like where you are, change it. Youfre not a tree."),
 ("All your dreams can come true if you have the courage to pursue them. "),
-("When you give up, thatfs when the game is over.");
-
+-- ("When you give up, thatfs when the game is over.");
+("Things will work out."),
+("You did a good job."),
+("Keep it up!"),
+("Just do it!"),
 
 -- replies table
 CREATE TABLE IF NOT EXISTS REPLIES (TARGET_WORD TEXT NOT NULL, REPLY_WORD TEXT NOT NULL,REPLYID INTEGER  PRIMARY KEY AUTOINCREMENT);
-INSERT INTO REPLIES(TARGET_WORD, REPLY_WORD) VALUES("hello", "Oh! hello!"),("wheather", "sunny"), ("tired","good luck!");
+INSERT INTO REPLIES(TARGET_WORD, REPLY_WORD)
+VALUES("pal-puppy", "What's up? My best friend."),
+-- ("wheather", "sunny"),
+("tired","good luck!");
 
 CREATE TABLE IF NOT EXISTS USERS (DIALY_MODE_FLAG INTEGER NOT NULL, USERID TEXT PRIMARY KEY);
