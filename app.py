a506f9ab89b28a011bc6340147db26a26bba047e
bonjob36 <yasui939714@gmail.com>
2022-09-15 11:03:17 +0900

    adminページ作成中

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, render_template
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -18,70 +18,114 @@ import random
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ['YOUR_CHANNEL_ACCESS_TOKEN'])
-handler = WebhookHandler(os.environ['YOUR_CHANNEL_SECRET'])
+# line_bot_api = LineBotApi(os.environ['YOUR_CHANNEL_ACCESS_TOKEN'])
+# handler = WebhookHandler(os.environ['YOUR_CHANNEL_SECRET'])
+
+line_bot_api = LineBotApi("xx")
+handler = WebhookHandler("xx")
 
 @app.route("/")
 def test():
     return "<h1>Tests</h1>"
 
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+# @app.route("/callback", methods=['POST'])
+# def callback():
+#     # get X-Line-Signature header value
+#     signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+#     # get request body as text
+#     body = request.get_data(as_text=True)
+#     app.logger.info("Request body: " + body)
 
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
+#     # handle webhook body
+#     try:
+#         handler.handle(body, signature)
+#     except InvalidSignatureError:
+#         print("Invalid signature. Please check your channel access token/channel secret.")
+#         abort(400)
 
-    return 'OK'
+#     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    con = sqlite3.connect('tables.db')
-    message = is_matched_full_text(event.message.text, con)
-    if message != "":
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=message))
-    else:
-        print("no match")
-    con.close()
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     con = sqlite3.connect('tables.db')
+#     message = is_matched_full_text(event.message.text, con)
+#     if message != "":
+#         line_bot_api.reply_message(
+#             event.reply_token,
+#             TextSendMessage(text=message))
+#     else:
+#         print("no match")
+#     con.close()
     
-    return 'OK'
+#     return 'OK'
 
-#プッシュメッセージ
-@app.route("/send")
-def push_message():
+# #プッシュメッセージ
+# @app.route("/send")
+# def push_message():
+#     con = sqlite3.connect('tables.db')
+#     cur = con.cursor()
+#     messages = cur.execute('''SELECT * FROM MESSAGES''').fetchall()
+#     line_bot_api.broadcast([TextSendMessage(text=random.choice(messages)[0])])
+#     print(random.choice(messages)[0])
+#     con.close()
+
+#     return 'OK'
+
+#adminサイト
+@app.route("/admin")
+def form():
     con = sqlite3.connect('tables.db')
     cur = con.cursor()
-    messages = cur.execute('''SELECT * FROM MESSAGES''').fetchall()
-    line_bot_api.broadcast([TextSendMessage(text=random.choice(messages)[0])])
-    print(random.choice(messages)[0])
+    messages = cur.execute(
+        '''SELECT MESSAGEID, MESSAGE FROM MESSAGES''').fetchall()
     con.close()
 
-    return 'OK'
+    return render_template('form.html', messages = messages)
 
-def is_matched_full_text(message, con):
-    cur = con.cursor()
-    reply_message = cur.execute('''SELECT REPLY_WORD FROM REPLIES WHERE TARGET_WORD=? ''', [message]).fetchone()
-    if reply_message is None:
-        return ""
-    else:
-        return reply_message[0]
+#adminサイト  格言追加処理
+@app.route('/register', methods = ['POST'])
+def confirm():
+    if request.method == 'POST':
+        result = request.form
+        con = sqlite3.connect('tables.db')
+        cur = con.cursor()
+        messages = cur.execute('''INSERT INTO MESSAGES(MESSAGE) VALUES(?)''', (result.getlist('register')[0],))
+        con.commit()
+        con.close()
+        # print(result.getlist('register')[0])
+        return form()
+
+
+
+#adminサイト  特定のキーワード
+@app.route('/register-key', methods = ['POST'])
+def keyregister():
+    if request.method == 'POST':
+        result = request.form
+        con = sqlite3.connect('tables.db')
+        cur = con.cursor()
+        messages = cur.execute('''INSERT INTO MESSAGES(MESSAGE) VALUES(?)''', (result.getlist('register')[0],))
+        con.commit()
+        con.close()
+        # print(result.getlist('register')[0])
+        return form()
+
+
+
+# def is_matched_full_text(message, con):
+#     cur = con.cursor()
+#     reply_message = cur.execute('''SELECT REPLY_WORD FROM REPLIES WHERE TARGET_WORD=? ''', [message]).fetchone()
+#     if reply_message is None:
+#         return ""
+#     else:
+#         return reply_message[0]
 
 
 if __name__ == "__main__":
     con = sqlite3.connect('tables.db')
-    print(is_matched_full_text("hello",con))
-    print(is_matched_full_text("ooo", con))
+    # print(is_matched_full_text("hello",con))
+    # print(is_matched_full_text("ooo", con))
 
     con.close()
